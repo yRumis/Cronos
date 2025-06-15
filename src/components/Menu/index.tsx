9c1fc3a615d6376d9be6d368235a1220563b0466
Ramon Silva <ramon.silva109@gmail.com>
2025-06-15 20:40:35 -0300

    adicionando tema dinamico..

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,49 @@
 import { ClockFading, HistoryIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useState } from "react";
 
 export function Menu() {
+
+  type avaibleTheme = "light" | "dark";
+
+  const [theme, setTheme] = useState<avaibleTheme>("dark");
+  
+  function handleThemeChange(e: React.MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault();
+    console.log(Date.now());
+    setTheme(e => {
+      const newTheme = e === "dark" ? "light" : "dark";
+      return newTheme;
+    })
+  }
   return (
+    
     <>
       <nav className={styles.menu}>
-        <a href="#" className={styles.menuLink}>
+        <h1>{theme}</h1>
+        <a href="#" className={styles.menuLink} 
+        aria-label="Ir para a home"
+        title="ir para a home">
           <ClockFading />
         </a>
 
-         <a href="#" className={styles.menuLink}>
-          <HistoryIcon />
+         <a href="#" className={styles.menuLink}
+          aria-label="ver historico"
+          title="ver historico"
+        >
+          <HistoryIcon/>
         </a>
 
-         <a href="#" className={styles.menuLink}>
+         <a href="#" className={styles.menuLink}
+          aria-label="Configuracoes"
+          title="Configuracoes">
           <SettingsIcon />
         </a>
 
-         <a href="#" className={styles.menuLink}>
+         <a href="#" className={styles.menuLink}
+          aria-label="mudar tema"
+          title="mudar tema"
+          onClick={(e) => {handleThemeChange(e)}}>
           <SunIcon />
         </a>
       </nav>
