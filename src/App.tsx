12fb36a6de5f66b59c9cb213811a6bf0f0ec5f43
Ramon Silva <ramon.silva109@gmail.com>
2025-07-01 23:44:48 -0300

    definindo modelos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { Home } from "./pages/Home";
-import { NotFound } from "./pages/NotFound";
-
 import "./styles/theme.css";
 import "./styles/global.css";
-import { AboutPomodoro } from "./pages/AboutPomodoro";
 
 export function App() {
   return  <Home />;
